d22c6ff9de721c9e7acbc9b7650a55f786c7bfa9
rfaross <rico.faross@mlp.de>
2021-02-23 08:31:47 +0100

    First Components

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
 ReactDOM.render(
-    <App subject="Clarice" />,
+    <App subject="Clarice" tasks={DATA} />,
   document.getElementById('root')
 );
 
